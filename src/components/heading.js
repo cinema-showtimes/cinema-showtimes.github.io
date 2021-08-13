9ae9c1a434193b842f5c419a674316047391bf58
Hemang Rajyaguru <hemdagem@gmail.com>
2021-08-13 23:01:18 +0100

    added tabs

---
+++ src/components/heading.js
@@ -0,0 +1,9 @@
+import * as React  from "react"
+
+const Heading = (props) => {
+    return (
+      <h1>{props.title}</h1>
+    )
+  }
+
+  export default Heading
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,44 @@
-import * as React from "react"
+import * as  React from "react"
 import { graphql } from "gatsby"
 import "/src/styles/main.scss"
+import Heading from "/src/components/heading"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.scss';
 
 // markup
 const IndexPage = ({ data }) => {
 
-
   return (
     <main>
       <title>BFI IMAX Films</title>
-      <h1>
-        BFI IMAX Films
-      </h1>
+
+      <Heading title="BFI IMAX Films" />
+
       {data.allFilmsYaml.edges.map(item => (
         <div key={item.node.id} className="movie-card">
           <div className="poster">
             <img src="https://via.placeholder.com/256x384" />
           </div>
           <div>
-            <div class="title"><h2>{item.node.title}</h2></div>
-            {item.node.showtimes.map(showtime => (
-              <div key={showtime}>
-                <div>{showtime.date}</div>
-                <div>
+
+
+            <div className="title"><h2>{item.node.title}</h2></div>
+            <Tabs>
+              <TabList>
+                {item.node.showtimes.map(showtime => (
+                    <Tab key={showtime.id}>{showtime.date}</Tab>
+                ))}
+              </TabList>
+
+              {item.node.showtimes.map(showtime => (
+                <TabPanel key={showtime.id}>
                   {showtime.times.map(time => (
-                    <a target="_blank" href={"https://www.odeon.co.uk/ticketing/seat-picker/?showtimeId=" + time.bookingid}>{time.time}</a>
+                  <a key={time.bookingid} target="_blank" href={"https://www.odeon.co.uk/ticketing/seat-picker/?showtimeId=" + time.bookingid}>{time.time}</a>
                   ))}
-                </div>
-              </div>
-            ))}
+                </TabPanel>
+              ))}
+            </Tabs>
+
           </div>
         </div>
       ))
