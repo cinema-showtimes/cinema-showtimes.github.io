245d636563f11e2a75838d12ae64ad3a0cf78cd9
Hemang Rajyaguru <hemdagem@gmail.com>
2021-10-13 12:48:24 +0100

    updated tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
               // This object is used for configuration specific to this plugin
               pluginConfig: {
                 // Puts tracking script in the head instead of the body
-                head: false,
+                head: true,
                 // Setting this parameter is also optional
                 respectDNT: false,
                 // Avoids sending pageview hits from custom paths
