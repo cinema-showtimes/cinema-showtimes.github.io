a7fe95be9c69ba97e5df058ee8474daa46db5846
Hemang Rajyaguru <hemdagem@gmail.com>
2021-11-26 23:41:33 +0000

    added film poster design

--- src/stories/FilmPoster.stories.jsx
+++ src/stories/FilmPoster.stories.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import FilmPoster from "/src/components/film-poster"
+import { withDesign } from 'storybook-addon-designs'
 export default {
   component: FilmPoster,
   title: "Components/FilmPoster",
+  decorators: [withDesign],
 }
 const Template = args => <FilmPoster {...args} />
 export const Default = Template.bind({})
@@ -10,3 +12,11 @@ Default.args = {
   src:"https://image.tmdb.org/t/p/w500//ziEuG1essDuWuC5lpWUaw1uXY2O.jpg",
   alt:"John Wick: Chapter 3 - Parabellum",
 }
+
+Default.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/G1aaQplpyvNjr2HpkXwxRx/IMAX-Movie-Release?node-id=5%3A5',
+  },
+}
+
